8017cf58ce70ec8b6cec10de131b378ca129e5d5
AracelyHernandez <aracely.hernandezhr@uanl.edu.mx>
2026-08-23 16:20:41 -0600

    ListasEj3

---
+++ ListasEj3.py
@@ -0,0 +1,7 @@
+
+artistas = ["Bandalos chinos","Latin mafia","Harry style","Empire of the sun","Rusowky"]
+
+artistas.extend(["Nskq","One direction","Babasonicos","Alanis yuki","Caifanes"])
+
+for i, artista in enumerate(artistas):
+    print(i + 1, artista)
