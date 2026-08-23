d3c16346cf6ab5fe59fd1674574c87a3b21deaf0
AracelyHernandez <aracely.hernandezhr@uanl.edu.mx>
2026-08-23 16:36:06 -0600

    Delete ListasEj.py

--- ListasEj.py
+++
@@ -1,7 +0,0 @@
-
-artistas = ["Bandalos chinos","Latin mafia","Harry style","Empire of the sun","Rusowky"]
-
-artistas.reverse()
-
-for i, artistas in enumerate(artistas):
-    print(i + 1, artistas)
