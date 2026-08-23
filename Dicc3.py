7617da948fbe640a409cc6875236c1b4c6284235
AracelyHernandez <aracely.hernandezhr@uanl.edu.mx>
2026-08-23 17:52:35 -0600

    Diccionario3

---
+++ Dicc3.py
@@ -0,0 +1,12 @@
+Usuarios = {"Nombre": ["Elizabeth", "Jesus", "Fatima", "Virgilio"],
+    "Edad": [19, 22, 18, 20],
+    "Estado civil": ["Soltera", "Casado", "Casada", "Soltero"],
+    "Color fav": ["Azul", "Morado", "Negro", "Verde"],
+    "Comida fav": ["Pizza", "Hamburguesa", "Tacos", "Ensalada"]
+}
+
+print(Usuarios["Nombre"][1])
+print(Usuarios["Edad"][3])
+print(Usuarios["Estado civil"][2])
+print(Usuarios["Color fav"][0])
+print(Usuarios["Comida fav"][3])
