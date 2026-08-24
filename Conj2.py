f0f35d9d6e9b45a334327c91ca0c7d3ac3a7a773
AracelyHernandez <aracely.hernandezhr@uanl.edu.mx>
2026-08-23 19:09:04 -0600

    Conjunto2

---
+++ Conj2.py
@@ -0,0 +1,6 @@
+set_colores = set(["Rojo", "Azul", "Verde", "Amarillo", "Naranja"])
+set_frutas = set(["Manzana", "Banana", "Naranja", "Fresa", "Uva"])
+
+set_interseccion = set_colores.intersection(set_frutas)
+print("Intersección de colores y frutas:", set_interseccion)
+
