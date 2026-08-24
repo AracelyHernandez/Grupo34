8b2bb64fbb158409acf14c6cf9ff7932c94be6b1
AracelyHernandez <aracely.hernandezhr@uanl.edu.mx>
2026-08-23 19:30:02 -0600

    Tupla2

---
+++ Tupla2.py
@@ -0,0 +1,8 @@
+colores = ["rojo", "negro", "verde", "blanco", "morado", "azul", "amarillo"]
+colores2 = ("rojo", "verde", "azul")
+
+print("El primer color es: " + colores[4])
+
+print("\nLos colores disponibles son:")
+for color in colores:
+    print(f'- {color}')
